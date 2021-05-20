8394fa80fb6918110f6eecc1ee1574454c432e2a
dmitryZ <zyumalin@bk.ru>
2021-05-20 06:54:04 +0300

    Refactoring

--- src/test/java/tests/PracticeFormTest.java
+++ src/test/java/tests/PracticeFormTest.java
@@ -22,56 +22,59 @@ public class PracticeFormTest {
 
     @Test
     void successfulPracticeForm() {
-        String firstName = "Dmitry";
-        String lastName = "Rejman";
-        String userEmail = "bgdt@gmail.com";
-        String userNumber = "9527776556";
-        String currentAdress = "Unter den Linden";
+        String firstName = "Dmitry",
+                lastName = "Rejman",
+                userEmail = "bgdt@gmail.com",
+                userNumber = "9527776556",
+                currentAdress = "Unter den Linden",
+                picture = "src/img/qa.jpg";
 
         open("https://demoqa.com/automation-practice-form");
 
         // input Name
-        $("[id=firstName]").setValue(firstName);
-        $("[id=lastName]").setValue(lastName);
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
 
         // input Email
-        $("[id=userEmail]").setValue(userEmail);
+        $("#userEmail").setValue(userEmail);
 
         // select Gender
         $(byText("Male")).click();
 
         // input Mobile
-        $("[id=userNumber]").setValue(userNumber);
+        $("#userNumber").setValue(userNumber);
 
         // select Date of Birth
-        $("[id=dateOfBirthInput]").click();
+        $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("July");
         $(".react-datepicker__year-select").selectOption("1995");
         $("[aria-label='Choose Wednesday, July 5th, 1995']").click();
 
         // input Subjects
-        $("[id=subjectsInput]").setValue("Computer Science").pressEnter();
+        $("#subjectsInput").setValue("Computer Science").pressEnter();
 
         // select Hobbies
         $(byText("Sports")).click();
 
         // select Picture
-        $("[id=uploadPicture]").uploadFile(new File("src/img/qa.jpg"));
+        $("#uploadPicture").uploadFile(new File(picture));
 
         // input Current Address
         $("[placeholder=\"Current Address\"]").setValue(currentAdress);
 
         // select state and city
-        $("[id=state]").click();
+        $("#state").click();
         $(byText("NCR")).click();
-        $("[id=city]").click();
+        $("#city").click();
         $(byText("Gurgaon")).click();
         $(byText("Submit")).click();
 
         // check Table
-        $(".table-responsive").shouldHave(text(firstName), text(lastName),
-                text(userEmail), text("Male"), text(userNumber), text("05 July,1995"),
-                text("Computer Science"), text("Sports"), text("qa.jpg"),
-                text("NCR"), text("Gurgaon"));
+        $(".table-responsive").shouldHave(
+                text(firstName), text(lastName),
+                text(userEmail), text("Male"),
+                text(userNumber), text("05 July,1995"),
+                text("Computer Science"), text("Sports"),
+                text("qa.jpg"), text("NCR"), text("Gurgaon"));
     }
 }
